b6db6f11908aaf6a3c0492319e9c4f72cf1170db
root <ianperry99@gmail.com>
2019-01-13 00:52:16 -0500

    update

--- modules/processlock.py
+++ modules/processlock.py
@@ -9,60 +9,66 @@ import socket
 host_name = socket.gethostname()
 log = logging.getLogger(name=host_name)
 
+
+class plock:
+    def __init__(self):
+        if os.access('/run', os.W_OK) and os.path.isdir('/run'):
+            lpath = '/run'
+        else:
+            log.critical('Cannot find a valid place to put the lockfile. Exiting')
+            exit(1)
+
+        self.ppid = str(os.getpid())
+        self.progname =  os.path.basename(sys.argv[0])
+        self.lockfile = f'{lpath}/{self.progname}.pid'
+        if not os.path.isfile(self.lockfile):
+            os.mknod(self.lockfile, mode=0x600)
+        self.lock_handle = open(self.lockfile, 'w')
+
+    def lock(self):
+        def aquireLock(self):
+            try:
+                fcntl.lockf(self.lock_handle, fcntl.LOCK_EX | fcntl.LOCK_NB)
+            except IOError:
+                return False
+            except:
+                log.exception('General error trying to lock process to file {}. exiting.'.format(elf.ockfile))
+                exit(1)
+            else:
+                log.debug('Process has been locked to file {} with PID [{}]'.format(self.lockfile, self.ppid))
+                return True
+        if aquireLock(self):
+            self.lock_handle.write(self.ppid)
+            self.lock_handle.close()
+            atexit.register(self.unlock)
+            return True
+        else:
+            # do pid checking
+            log.warning('Process is locked. Another instance is running.')
+    #        time.sleep(5)
+    #    for numtries in range(1):
+    #        if aquireLock():
+    #            lock_handle.write(ppid)
+    #            atexit.register(unlock)
+    #            return True
+    #        else:
+    #            time.sleep(5)
+        log.error('Could not obtain process lock. Exiting')
+        exit(1)
+
+
+    def unlock(self):
+        try:
+            fcntl.flock(self.lock_handle, fcntl.LOCK_UN)
+            self.lock_handle.close()
+        except:
+            pass
+        if os.path.isfile(self.lockfile):
+            os.remove(self.lockfile)
+
 def cleanName(filename):
     filename = os.path.basename(filename)
     filename = filename.rsplit('.', 1)[0]
     return filename
 
 
-def plock():
-    ppid = str(os.getpid())
-
-    def aquireLock():
-        if not os.path.isfile(lockfile):
-            os.mknod(lockfile, mode=0o600)
-        try:
-            fcntl.lockf(lock_handle, fcntl.LOCK_EX | fcntl.LOCK_NB)
-        except IOError:
-            return False
-        except:
-            log.exception('General error trying to lock process to file {}. exiting.'.format(lockfile))
-            exit(1)
-        else:
-            log.debug('Process has been locked to file {} with PID [{}]'.format(lockfile, ppid))
-            return True
-    if aquireLock():
-        # lock_handle = open(lockfile, 'w')
-        lock_handle.write(ppid)
-        # lock_handle.close()
-        atexit.register(unlock)
-        return True
-    else:
-        # do pid checking
-        log.warning('Process is locked. Another instance is running.')
-#        time.sleep(5)
-#    for numtries in range(1):
-#        if aquireLock():
-#            lock_handle.write(ppid)
-#            atexit.register(unlock)
-#            return True
-#        else:
-#            time.sleep(5)
-    log.error('Could not obtain process lock. Exiting')
-    exit(1)
-
-
-def unlock():
-    fcntl.flock(lock_handle, fcntl.LOCK_UN)
-    lock_handle.close()
-    if os.path.isfile(lockfile):
-        os.remove(lockfile)
-
-if os.access('/tmp', os.W_OK) and os.path.isdir('/tmp'):
-    lpath = '/tmp'
-else:
-    log.critical('Cannot find a valid place to put the lockfile. Exiting')
-    exit(1)
-
-lockfile = f'{lpath}/{cleanName(sys.argv[0])}.lock'
-lock_handle = open(lockfile, 'w')
