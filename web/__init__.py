989a0cd515c91366ffe8affbb4c82a6abb83f56f
root <ianperry99@gmail.com>
2019-01-20 21:56:55 -0500

    log  updates

--- web/__init__.py
+++ web/__init__.py
@@ -13,7 +13,9 @@ def create_app(config_object):
     app.config.from_object(config_object)
     app.logger.disabled = True
     log = logging.getLogger('werkzeug')
-    log.disabled = True
+    log.addHandler(logging.FileHandler('/var/log/access.log'))
+
+    #log.disabled = True
     register_extensions(app)
     register_blueprints(app)
     # security.init_app(app, register_blueprint=True)
