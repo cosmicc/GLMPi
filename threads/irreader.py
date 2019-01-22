079ceeed2bb484837eb20db9c1534db7b9800dd0
root <ianperry99@gmail.com>
2019-01-21 22:51:43 -0500

    ir reader

--- threads/irreader.py
+++ threads/irreader.py
@@ -1,30 +1,33 @@
 from time import sleep
-from datetime import datetime
 from configparser import ConfigParser
-from modules.extras import str2bool, End
+from modules.extras import End
 from loguru import logger as log
 import socket
 
 config = ConfigParser()
 config.read('/etc/glmpi.conf')
-loopdelay = float(config.get('irreader', 'loopdelay'))
-motionlight = str2bool(config.get('motion', 'light'))
 
 
 class irReader():
     def __init__(self):
         log.info(f'Initilizing IR sensor reciever')
+        self.lastcode = None
         try:
             self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-            sock.connect("/var/run/lirc/lircd")
+            self.sock.connect("/var/run/lirc/lircd")
         except:
             log.exception('Error initilizing IR sensor')
 
     def listen(self):
-        data = sock.recv(128)
+        data = self.sock.recv(128)
         data = data.strip()
         if data:
-            return data.split()[2].decode()
+            rdata = data.split()[2].decode()
+            if rdata == self.lastcode:
+                return None
+            else:
+                self.lastcode = rdata
+                return rdata
 
 
 def irreader_thread():
@@ -32,7 +35,9 @@ def irreader_thread():
     ir = irReader()
     while True:
         try:
-            sleep(loopdelay)
+            ircode = ir.listen()
+            if ircode == 'KEY_RED':
+                log.warning('RED!!!!')
         except:
             log.exception(f'Exception in Motion Detection Thread', exc_info=True)
             End('Exception in Motion Detection thread')
