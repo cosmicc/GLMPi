a4a5dd7cf991aaaf62dd7e5466fd2de4a78d954f
root <ianperry99@gmail.com>
2019-01-21 23:06:23 -0500

    ir reader

--- threads/irreader.py
+++ threads/irreader.py
@@ -36,9 +36,80 @@ def irreader_thread():
     while True:
         try:
             ircode = ir.listen()
-            if ircode == 'KEY_RED':
-                log.warning('RED!!!!')
+            if ircode == 'KEY_POWER':
+                log.info('POWER ON recieved from IR')
+
+            elif ircode == 'KEY_POWER2':
+                log.info('POWER OFF recieved from IR')
+
+            elif ircode == 'KEY_UP':
+                log.info('BRIGHT UP recieved from IR')
+
+            elif ircode == 'KEY_DOWN':
+                log.info('BRIGHT DOWN recieved from IR')
+
+            elif ircode == 'KEY_RED':
+                log.info('RED recieved from IR')
+
+            elif ircode == 'KEY_GREEN':
+                log.info('GREEN recieved from IR')
+
+            elif ircode == 'KEY_BLUE':
+                log.info('BLUE recieved from IR')
+
+            elif ircode == 'KEY_W':
+                log.info('WHITE recieved from IR')
+
+            elif ircode == 'KEY_PROG1':
+                log.info('PROG1 recieved from IR')
+
+            elif ircode == 'KEY_PROG2':
+                log.info('PROG2 recieved from IR')
+
+            elif ircode == 'KEY_PROG3':
+                log.info('PROG3 recieved from IR')
+
+            elif ircode == 'KEY_PROG4':
+                log.info('PROG4 recieved from IR')
+
+            elif ircode == 'KEY_F1':
+                log.info('COLOR1 recieved from IR')
+
+            elif ircode == 'KEY_F2':
+                log.info('COLOR2 recieved from IR')
+
+            elif ircode == 'KEY_F3':
+                log.info('COLOR3 recieved from IR')
+
+            elif ircode == 'KEY_F4':
+                log.info('COLOR4 recieved from IR')
+
+            elif ircode == 'KEY_F5':
+                log.info('COLOR5 recieved from IR')
+
+            elif ircode == 'KEY_F6':
+                log.info('COLOR6 recieved from IR')
+
+            elif ircode == 'KEY_F7':
+                log.info('COLOR7 recieved from IR')
+
+            elif ircode == 'KEY_F8':
+                log.info('COLOR8 recieved from IR')
+
+            elif ircode == 'KEY_F9':
+                log.info('COLOR9 recieved from IR')
+
+            elif ircode == 'KEY_F10':
+                log.info('COLOR10 recieved from IR')
+
+            elif ircode == 'KEY_F11':
+                log.info('COLOR11 recieved from IR')
+
+            elif ircode == 'KEY_F12':
+                log.info('COLOR12 recieved from IR')
+            else:
+                log.debug(f'INVALID CODE [{ircode}] recieved from IR')
         except:
-            log.exception(f'Exception in Motion Detection Thread', exc_info=True)
-            End('Exception in Motion Detection thread')
-    End('Motion Detection thread loop ended prematurely')
+            log.exception(f'Exception in IR reader Thread', exc_info=True)
+            End('Exception in IR reader thread')
+    End('IR reader thread loop ended prematurely')
