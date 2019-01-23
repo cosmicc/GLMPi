2677b3b739033b594e8b65b6687d6a384b32c060
root <ianperry99@gmail.com>
2019-01-22 20:44:13 -0500

    access log

--- web/__init__.py
+++ web/__init__.py
@@ -6,16 +6,17 @@ import logging
 config = ConfigParser()
 config.read('/etc/glmpi.conf')
 ismaster = str2bool(config.get('master_controller', 'enabled'))
-
+access_log = str2bool(config.get('general', 'access_log'))
 
 def create_app(config_object):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(config_object)
     app.logger.disabled = True
     log = logging.getLogger('werkzeug')
-    log.addHandler(logging.FileHandler('/var/log/access.log'))
-
-    #log.disabled = True
+    if access_log:
+        log.addHandler(logging.FileHandler('/var/log/access.log'))
+    else:
+        log.disabled = True
     register_extensions(app)
     register_blueprints(app)
     # security.init_app(app, register_blueprint=True)
